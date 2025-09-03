984f98d4a771f6aeb5f81b31ef5add2f72d53301
Ashish <kumarmaliashish0@gmail.com>
2025-09-03 18:38:23 +0530

    cretae Form processing

---
+++ client/src/components/Inputs/Input.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+const Input = ({value, onChange, placeholder, label, type}) => {
+    const[showpassword, setShowPassword] = useState(false)
+  return (
+    <div>
+        input
+    </div>
+  )
+}
+
+export default Input
--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom'
+import Input from '../../components/Inputs/Input';
 
 const Login = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null)
+
+  const navigate = useNavigate();
+
+  // handleLogin
+  const handleLogin = async (e)=>{
+
+  }
+
   return (
-    <div>login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input type="text" value={email} onChange={({target}) => setEmail(target.value)} label="Email Address" placeholder='Entermail@mail.com' />
+        </form>
+
+      </div>
+    </AuthLayout>
   )
 }
 
